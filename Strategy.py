664f4afc5bb478200f6f3e465823e9faec68621d
laughingbud <119120156+laughingbud@users.noreply.github.com>
2024-10-15 16:15:30 +0100

    Update Strategy.py

--- Strategy.py
+++ Strategy.py
@@ -121,6 +121,8 @@ class Strategy:
         return cci, cci_signal
 
     def macd_signal(self,data,window_slow: int=26,window_fast: int=1):
+        if window_fast >= window_slow:
+            raise ValueError("Fast window should be smaller than slow window")
         macd = ta.trend.MACD(data['close'],window_slow,window_fast).macd
         macd_calc = ta.trend.MACD(data['close'],window_slow,window_fast).macd_signal()
         macd_signal = np.where(macd > macd_calc, 1, np.where(macd < macd_calc, -1, 0))
